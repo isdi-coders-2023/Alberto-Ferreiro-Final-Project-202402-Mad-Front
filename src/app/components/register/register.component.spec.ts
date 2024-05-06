ac00343ecad12d018276fc2acdbc63c38bd06065
FerreiroAlberto <homelandz@gmail.com>
2024-05-06 11:04:28 +0200

    Files relocation

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,26 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
-// import { of } from 'rxjs';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { UsersRepoService } from '../../services/users.repo.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-  // const httpClientMock = {
-  //   get: jasmine.createSpy('get').and.returnValue(of()),
-  // };
-  // const repoServiceMock = {
-  //   getById: jasmine
-  //     .createSpy('getById')
-  //     .and.returnValue(
-  //       of({ id: '1234567890', name: 'Axl', email: 'axl@example.com' })
-  //     ),
-  //   create: jasmine.createSpy('create').and.returnValue(of()),
-  // };
+  const httpMock = {
+    get: jasmine.createSpy('get').and.returnValue(of()),
+  };
+  const repoMock = {
+    getById: jasmine
+      .createSpy('getById')
+      .and.returnValue(
+        of({ id: '1234567890', name: 'Axl', email: 'axl@example.com' })
+      ),
+    create: jasmine.createSpy('register').and.returnValue(
+      of({
+        id: '1234567890',
+        email: 'axl@rose.com',
+        name: 'Axl',
+        policies: [],
+      })
+    ),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RegisterComponent],
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: httpMock,
+        },
+        { provide: UsersRepoService, useValue: repoMock },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
